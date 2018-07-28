97b3302032dd5448d16306e05acd82ade306daeb
betarunex <betarunex@gmail.com>
2018-07-28 22:10:28 +0200

    Simple coordinates for triangle

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ if(!gl){
 // Change the background colour of our canvas
 gl.clearColor(0.5,0.6,0.9,1); // rgb alpha 0.0-1.0 (Kind of default colour selected to this)
 gl.clear(gl.COLOR_BUFFER_BIT) // Clear the canvas (with selected cleared colour)
+
+var trianglePoints = [
+    -0.6,0,
+    0,-0.8,
+    0.5,1
+];
